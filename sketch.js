e68bf27cc208032b628e935a134c341f996f82e1
ChloeM08 <71110736+ChloeM08@users.noreply.github.com>
2020-11-02 18:51:28 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canvas, database;
+
+var playerCount;
+var gameState = 0;
+
+var player, game, form;
+
+function setup(){
+    canvas = createCanvas(400,400);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+
+}
